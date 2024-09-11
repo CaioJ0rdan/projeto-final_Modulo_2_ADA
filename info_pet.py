44e45b589735029943ca0ee1b5ed70c4d92bf5f2
Caio J0rdan <caio10jordan@alu.ufc.br>
2024-09-10 23:09:48 -0300

    feat: Adicionando Funcao de Mostrar Pet

--- info_pet.py
+++ info_pet.py
@@ -26,12 +26,31 @@ def coletar_informacoes_pet():
                 break
         except ValueError:
             print("Por favor, insira um número válido para o peso.")
+    
+    # Exibindo as informações coletadas 
+    # pela funcao mostrar_informacoes_pet
+    petdata = [nome,idade,peso]
+    mostrar_informacoes_pet(petdata)
+    
 
-    # Exibindo as informações coletadas
+    # Retornando as informacoes dos pets para serem armazenadas
+    return [nome,idade,peso]
+
+
+# Funcao para mostrar as informacoes do pet sem coleta-las
+def mostrar_informacoes_pet(petdata):
+    # Exibindo as informações do pet
     print("\nInformações do pet:")
-    print(f"Nome: {nome}")
-    print(f"Idade: {idade} anos")
-    print(f"Peso: {peso} kg")
+    print(f"Nome: {petdata[0]}")
+    print(f"Idade: {petdata[1]} anos")
+    print(f"Peso: {petdata[2]} kg")
+    
+
+
+
+
+# Criando array para guardar informacoes dos pets 
+Pets = []
 
 # Chama a função para coletar e exibir as informações do pet
-coletar_informacoes_pet()
+Pets.append(coletar_informacoes_pet())
